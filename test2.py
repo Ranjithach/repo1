612fab6437646ce3216a7d8bf456850a535bafcf
Ranjitha T N <ranjitha.tn@nsplustech.com>
2022-07-01 13:55:48 +0530

    added 2 files

---
+++ test2.py
@@ -0,0 +1,11 @@
+ Volume in drive C is OS
+ Volume Serial Number is 0490-1856
+
+ Directory of C:\test git
+
+01-07-2022  01:53 PM    <DIR>          .
+01-07-2022  01:53 PM    <DIR>          ..
+01-07-2022  01:53 PM               328 test1.txt
+01-07-2022  01:53 PM                 0 test2.py
+               2 File(s)            328 bytes
+               2 Dir(s)  369,756,811,264 bytes free
